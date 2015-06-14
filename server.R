56f760c1a8bfdf091f261a1bdb2e113b81e24018
chiahuaw <chiahua3@gmail.com>
2015-06-14 18:57:43 +0800

    改變版面配置，並新增由列表反定位建案地圖位置的功能。

--- server.R
+++ server.R
@@ -1,25 +1,41 @@
 
 library(shiny)
 library(dplyr)
-library(ggplot2)
-library(scales)
-library(ggmap)
 library(leaflet)
 
-f2 <- file("data/buildingGeo.csv",encoding="big5")
-building <- read.csv(f2,encoding="big5",stringsAsFactors=F)
+shinyServer(function(input, output,session) {
 
-shinyServer(function(input, output) {
-
-
+  f2 <- file("data/buildingGeo.csv",encoding="big5")
+  building <- read.csv(f2,encoding="big5",stringsAsFactors=F)
+  building<-mutate(building,發文日期=as.Date(發文日期)) %>% arrange(.,desc(發文日期))
   
   output$map<-renderLeaflet({
-    leaflet() %>%
+    maps<-leaflet() %>%
       addTiles() %>% 
       addMarkers(lng=building$lon,lat=building$lat,popup=paste("建號：",building$建號)) %>%
-      setView(lng=118.375,lat=24.445,zoom=11)
+      setView(lng=118.375,lat=24.445,zoom=12)
   })
   
+  observe({
+    if (is.null(input$goto))
+      return()
+    isolate({
+      map <- leafletProxy("map")
+      #map %>% clearPopups()
+      dist <- 0.0015
+      lat <- input$goto$lat
+      lng <- input$goto$lon
+      map %>% fitBounds(lng - dist, lat - dist, lng + dist, lat + dist)
+    })
+    })
+    output$ziptable <- DT::renderDataTable({
+      df <- building %>%
+        mutate(Action = paste('<a class="go-map" href="" data-lat="', lat, '" data-long="', lon, '">location</a>', sep=""))
+      action <- DT::dataTableAjax(session, df)
+      
+      DT::datatable(df, options = list(ajax = list(url = action)),escape = FALSE)
+    })
+    
   
   output$table<-renderDataTable(building) 
 
--- ui.R
+++ ui.R
@@ -4,13 +4,14 @@ library(dplyr)
 library(leaflet)
 
 shinyUI(navbarPage("金門縣新建案道路挖掘列管",
-                   tabPanel("位置檢視",
-                            titlePanel("新建案位置檢視"),
-                            navlistPanel("檢視模式選擇",
-                                         tabPanel("地圖檢視",leafletOutput("map")),
-                                         tabPanel("一覽表",dataTableOutput(outputId="table"))
-                                         )
-                            
+                   tabPanel("新建案位置檢視",
+                            tags$head(
+                              # Include our custom CSS
+                              includeScript("gomap.js")
+                              ),
+                          
+                            leafletOutput("map"),
+                            DT::dataTableOutput(outputId="ziptable")
+                            )
                    )
-)
-)
+        )
