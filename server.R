9cd0ea7ebbd82739b0e22a2be85ba433c4f341ee
chiahuaw <chiahua3@gmail.com>
2015-06-14 16:03:56 +0800

    改用 leaflet 做地圖表現。

--- server.R
+++ server.R
@@ -4,28 +4,22 @@ library(dplyr)
 library(ggplot2)
 library(scales)
 library(ggmap)
+library(leaflet)
 
 f2 <- file("data/buildingGeo.csv",encoding="big5")
 building <- read.csv(f2,encoding="big5",stringsAsFactors=F)
 
-p<-ggmap(get_map(location = c(lon = 118.375, lat = 24.445), zoom = 12, maptype = "terrain"))
-p2<-ggmap(get_map(location = c(lon = 118.240, lat = 24.430), zoom = 14, maptype = "terrain"))
-
 shinyServer(function(input, output) {
 
-  output$map1Plot <- renderPlot({
 
-    p+geom_point(data=building,aes(x=lon,y=lat),colour=as.numeric(substr(building$建號,1,3))/2)+
-      ggtitle("Where Building is")
-
+  
+  output$map<-renderLeaflet({
+    leaflet() %>%
+      addTiles() %>% 
+      addMarkers(lng=building$lon,lat=building$lat,popup=paste("建號：",building$建號)) %>%
+      setView(lng=118.375,lat=24.445,zoom=11)
   })
   
-  output$map2Plot <- renderPlot({
-    
-    p2+geom_point(data=building,aes(x=lon,y=lat),colour=as.numeric(substr(building$建號,1,3))/2)+
-      ggtitle("Where Building is")
-    
-  })
   
   output$table<-renderDataTable(building) 
 
--- ui.R
+++ ui.R
@@ -1,24 +1,15 @@
 
 library(shiny)
 library(dplyr)
+library(leaflet)
 
 shinyUI(navbarPage("金門縣新建案道路挖掘列管",
                    tabPanel("位置檢視",
                             titlePanel("新建案位置檢視"),
-#                             selectInput("years",
-#                                         "年度",
-#                                         choices=c(unique(format(dat$V1,format="%Y")),unique(format(rain$V1,format="%Y"))),#(c("2006","2007","2008","2009","2010","2011","2012","2013","2014","2015")),
-#                                         selected="2015"),
-#                             selectInput("month",
-#                                         "月份",
-#                                         choices=c(paste(0,seq(1:9),sep=""),"10","11","12"),
-#                                         selected="1"),
-#                             downloadButton('downloadData', '下載單年雨量CSV檔'), #下載單年雨量CSV
-                            navlistPanel("位置別",
-                                         tabPanel("大金門地區",plotOutput("map1Plot")),
-                                         tabPanel("烈嶼鄉地區",plotOutput("map2Plot")),
+                            navlistPanel("檢視模式選擇",
+                                         tabPanel("地圖檢視",leafletOutput("map")),
                                          tabPanel("一覽表",dataTableOutput(outputId="table"))
-                            )
+                                         )
                             
                    )
 )
